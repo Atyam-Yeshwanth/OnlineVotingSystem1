c9b8e96ddec32cab687ed95568d01cbac432959b
Atyam Yeshwanth <126558692+Atyam-Yeshwanth@users.noreply.github.com>
2024-01-03 13:35:06 +0530

    Add files via upload

---
+++ online voting system sql.sql
@@ -0,0 +1,9 @@
+create database voting_system;
+use voting_system;
+show tables;
+select * from voters;
+select * from voters_roles;
+select * from roles;
+select * from hibernate_sequence;
+select * from admin;
+
